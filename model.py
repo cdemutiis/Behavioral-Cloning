fbb3fce0fbafb5b5039c3427e3ee1bc714181b59
Claudio S. De Mutiis <claudiodemutiis@gmail.com>
2017-02-27 23:32:45 +0000

    add the first model without bugs

--- model.py
+++ model.py
@@ -13,8 +13,12 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 import cv2
 import numpy as np
 import sklearn
+from sklearn.utils import shuffle
 from keras.models import Sequential, Model, load_model
 from keras.layers import Cropping2D, Lambda
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 import matplotlib.pyplot as plt
 
 def generator(samples, batch_size=32):
@@ -45,26 +49,32 @@ row, col, ch = 160, 320, 3
 
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=(ch, row, col),
-        output_shape=(ch, row, col)))
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch)))
+#model.add(Cropping2D(cropping=((50,20), (0,0)),image_dim_ordering ))
+model.add(Convolution2D(24,5,5))
+model.add(Convolution2D(36,5,5))
+model.add(Convolution2D(48,3,3))
+model.add(Convolution2D(64,3,3))
+model.add(Flatten())
+#model.add(Dense(1164))
+#model.add(Dense(100))
+#model.add(Dense(50))
+#model.add(Dense(10))
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, samples_per_epoch= /
-            len(train_samples), validation_data=validation_generator, /
-            nb_val_samples=len(validation_samples), nb_epoch=5)
+history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5)
 
 ### print the keys contained in the history object
-print(history_object.history.keys())
+#print(history_object.history.keys())
 
 ### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
+#plt.plot(history_object.history['loss'])
+#plt.plot(history_object.history['val_loss'])
+#plt.title('model mean squared error loss')
+#plt.ylabel('mean squared error loss')
+#plt.xlabel('epoch')
+#plt.legend(['training set', 'validation set'], loc='upper right')
+#plt.show()
 
 model.save('my_model.h5')
