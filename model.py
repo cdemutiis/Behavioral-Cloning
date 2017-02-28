1f1ca214b77577b089251a39e5fe5511e61a1cac
Claudio S. De Mutiis <claudiodemutiis@gmail.com>
2017-02-28 21:29:26 +0000

    add the correct version of the model

--- model.py
+++ model.py
@@ -35,13 +35,17 @@ def generator(samples, batch_size=32):
                     name = './IMG/'+batch_sample[i].split('/')[-1]
                     image = cv2.imread(name)
                     angle = float(batch_sample[3])
-                    correction = 0.2 
+                    correction = 0.3 
                     if i == 1:
                         angle = angle + correction
                     if i == 2:
                         angle = angle - correction
                     images.append(image)
                     angles.append(angle)
+                    image_flipped = np.fliplr(image)
+                    angle_flipped = -angle
+                    images.append(image_flipped)
+                    angles.append(angle_flipped)
 
             X_train = np.array(images)
             y_train = np.array(angles)
@@ -56,7 +60,7 @@ row, col, ch = 160, 320, 3
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
 model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch)))
-#model.add(Cropping2D(cropping=((50,20), (0,0)),image_dim_ordering ))
+model.add(Cropping2D(cropping=((50,20), (0,0))))
 model.add(Convolution2D(24,5,5))
 model.add(MaxPooling2D((2, 2)))
 model.add(Activation('relu'))
@@ -70,14 +74,20 @@ model.add(Convolution2D(64,3,3))
 model.add(MaxPooling2D((2, 2)))
 model.add(Activation('relu'))
 model.add(Flatten())
-#model.add(Dense(1164))
+model.add(Dense(1164))
+model.add(Activation('relu'))
+#model.add(Dropout(0.5))
 model.add(Dense(100))
-#model.add(Dense(50))
+model.add(Activation('relu'))
+#model.add(Dropout(0.5))
+model.add(Dense(50))
+model.add(Activation('relu'))
 model.add(Dense(10))
+model.add(Activation('relu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=4)
 
 ### print the keys contained in the history object
 #print(history_object.history.keys())
